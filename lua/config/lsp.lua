58ef3c12cfa22f2192631212254791bbc8ef3ab2
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2025-06-13 23:05:04 +0200

    feat: Vue support for ts_ls

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -37,6 +37,24 @@ require("mason-lspconfig").setup({
 })
 
 
+vim.lsp.config('ts_ls', {
+    init_options = {
+        -- Enable Vue support
+        plugins = {
+            {
+                name = '@vue/typescript-plugin',
+                location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+                languages = { 'vue' },
+            },
+        },
+    },
+    filetypes = {
+        'typescript',          -- TypeScript files (.ts)
+        'javascript',          -- JavaScript files (.js)
+        'javascriptreact',     -- React files with JavaScript (.jsx)
+        'typescriptreact',     -- React files with TypeScript (.tsx)
+        'vue'                  -- Vue.js single-file components (.vue)
+    },
 })
 
 
