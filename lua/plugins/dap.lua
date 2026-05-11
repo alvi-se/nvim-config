4911177f62ac3eef5894b4c515e9b32d4a2cc83c
Alvise Favero <66018479+alvi-se@users.noreply.github.com>
2026-05-11 22:30:55 +0200

    chore: update nvim-pack-lock.json

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -1,11 +1,3 @@
-
-
-local dap_ok, dap = pcall(require, "dap")
-if not dap_ok then
-	print("nvim-dap not installed!")
-	return
-end
-
 require("dap").set_log_level("INFO") -- Helps when configuring DAP, see logs with :DapShowLog
 
 require("mason-nvim-dap").setup({
